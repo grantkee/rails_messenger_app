a9d094452057c192114c11e868b7d6498b54c7a0
grantkee <grant.d.kee@gmail.com>
2020-07-17 22:49:08 -0500

    first commit based on lesson with codecademy

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+# m1 = Message.create(user: "coolCat", content: "Heyyy BigG! Long time no chat!")
+# m2 = Message.create(user: "bigG", content: "Who is this? And why are you messaging me while I'm trying to code?")
+
+# m3 = Message.create(user: "coolCat", content: "Oh sNaP D: K3eP iT up D0od. uR g0nNa b3 a l33t hax0r 1 dAy xD")
+# m4 = Message.create(user: "bigG", content: "wooooooord")
+
+# m5 = Message.create(user: "coolCat", content: "bRo c0uLD i AsK eWe a Qu3St1on Br0??")
+
+# m6 = Message.create(user: "bigG", content: "Yes, but only if you promise to never type like that again...")
