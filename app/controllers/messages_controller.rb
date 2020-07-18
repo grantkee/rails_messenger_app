9f122343e1c9117be234697330ee7f9badb73bea
grantkee <grant.d.kee@gmail.com>
2020-07-17 23:41:15 -0500

    added new route for user to create message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,26 @@
 class MessagesController < ApplicationController
+  # show all messages
   def index
     @messages = Message.all
   end
+
+  # show new message
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to 'messages'
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def message_params
+      params.require(:message).permit(:user, :content)
+    end
+
 end
